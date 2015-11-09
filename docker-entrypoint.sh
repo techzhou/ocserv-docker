81122b3795d1ce49736b04e09d720dd76bbac2ee
zhouchao <yzlkester@gmail.com>
2015-11-09 11:13:51 +0800

    first init

---
+++ docker-entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ ! -f /etc/ocserv/server-key.pem ] || [ ! -f /etc/ocserv/server-cert.pem ]; then
+	CA_CN="VPN CA"
+	CA_ORG="Big Corp"
+	CA_DAYS=9999
+	SRV_CN="www.example.com"
+	SRV_ORG="HugeCopmany"
+	SRV_DAYS=9999
+
+	cd /etc/ocserv
+	certtool --generate-privkey --outfile ca-key.pem
+	cat > ca.tmpl <<-EOCA
+	cn = "$CA_CN"
+	organization = "$CA_ORG"
+	serial = 1
+	expiration_days = $CA_DAYS
+	ca
+	signing_key
+	cert_signing_key
+	crl_signing_key
+	EOCA
+	certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile ca.pem
+	certtool --generate-privkey --outfile server-key.pem 
+	cat > server.tmpl <<-EOSRV
+	cn = "$SRV_CN"
+	organization = "$SRV_ORG"
+	expiration_days = $SRV_DAYS
+	signing_key
+	encryption_key
+	tls_www_server
+	EOSRV
+	certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
+
+fi
+
+# Open ipv4 ip forward
+sysctl -w net.ipv4.ip_forward=1
+
+# Enable NAT forwarding
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+
+# Enable TUN device
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+
+# Run OpennConnect Server
+exec "$@"
+
